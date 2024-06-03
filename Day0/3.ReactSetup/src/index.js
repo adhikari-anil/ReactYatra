04c7b9e2a6027b6256860f9e848ead3a6aafec8a
Anil Adhikari <adhikarianil719@gmail.com>
2024-06-03 22:26:26 +0545

    Setup react app via webpack. In addition we make babelrc,webpack.config file..

---
+++ Day0/3.ReactSetup/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const child1 = React.createElement('h1', {}, "This is the normal one...");
+const child2 = React.createElement('p', {}, " Hello this is injecting using React...");
+
+const div = React.createElement('div',{}, [child1, child2]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
